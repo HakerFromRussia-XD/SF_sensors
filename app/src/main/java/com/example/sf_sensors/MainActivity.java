8d17af87d1824a7c4a8fe61650bb932edc9077ca
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-03-29 19:01:27 +0300

    выведен список датчиков

--- app/src/main/java/com/example/sf_sensors/MainActivity.java
+++ app/src/main/java/com/example/sf_sensors/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.sf_sensors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private SensorManager sensorManager;
+    private List<Sensor> deviceSensors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-//        sensorManager =
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+
+//        textView.setText(deviceSensors.toString());
+        printSensors();
+    }
+
+    private void printSensors() {
+        for (Sensor sensor : deviceSensors){
+            textView.setText(textView.getText()+"\n"+sensor.getName());
+        }
     }
 }
