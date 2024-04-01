fc5126440bb9a02cdc024d78b1f8643af33865a6
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-04-01 16:23:52 +0300

    шаг 6 чтение показателей датчика ускорения

--- app/src/main/java/com/example/sf_sensors/MainActivity.java
+++ app/src/main/java/com/example/sf_sensors/MainActivity.java
@@ -6,35 +6,40 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView textView;
+    private TextView x_tv, y_tv, z_tv;
     private SensorManager sensorManager;
-    private Sensor sensor;
+    private Sensor accelerometerSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        x_tv = findViewById(R.id.x_tv);
+        y_tv = findViewById(R.id.y_tv);
+        z_tv = findViewById(R.id.z_tv);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
-            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        } else {
-            System.err.println("нет гироскопа");
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){} else {
+            System.err.println("Датчик ускорения недоступен");
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float changedValue = event.values[0];
-        textView.setText(String.valueOf(changedValue));
-        System.err.println("отправили новые данные "+event.values[0]);
+        float changedValueX = event.values[0];
+        float changedValueY = event.values[1];
+        float changedValueZ = event.values[2];
+        x_tv.setText(String.valueOf(changedValueX));
+        y_tv.setText(String.valueOf(changedValueY));
+        z_tv.setText(String.valueOf(changedValueZ));
     }
 
     @Override
@@ -44,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         System.err.println("onResume()");
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
