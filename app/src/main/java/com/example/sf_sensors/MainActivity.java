3f5109134ce539214ce52ed36edd0fbd3c0f8972
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2024-04-11 17:43:07 +0300

    шаг 7 тест счётчика шагов

--- app/src/main/java/com/example/sf_sensors/MainActivity.java
+++ app/src/main/java/com/example/sf_sensors/MainActivity.java
@@ -13,33 +13,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView x_tv, y_tv, z_tv;
+    private TextView step_counter_tv, step_detector_tv;
     private SensorManager sensorManager;
-    private Sensor accelerometerSensor;
+    private Sensor stepCounter;
+    private int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        x_tv = findViewById(R.id.x_tv);
-        y_tv = findViewById(R.id.y_tv);
-        z_tv = findViewById(R.id.z_tv);
+        step_counter_tv = findViewById(R.id.step_counter_tv);
+        step_detector_tv = findViewById(R.id.step_detector_tv);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){} else {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        } else {
             System.err.println("Датчик ускорения недоступен");
+        }
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
+
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float changedValueX = event.values[0];
-        float changedValueY = event.values[1];
-        float changedValueZ = event.values[2];
-        x_tv.setText(String.valueOf(changedValueX));
-        y_tv.setText(String.valueOf(changedValueY));
-        z_tv.setText(String.valueOf(changedValueZ));
+        if (event.sensor == stepCounter) {
+            stepCount = (int) event.values[0];
+            step_counter_tv.setText("step counter = "+stepCount);
+        }
     }
 
     @Override
@@ -49,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         System.err.println("onResume()");
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
